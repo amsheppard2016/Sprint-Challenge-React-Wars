4c90d5093feb2ca449a4c8aea0016daf5e69342e
Ash W <amsheppard2016@gmail.com>
2020-02-10 21:52:30 -0500

    moved axios to app.js and deleted peopledata file

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,10 +1,35 @@
-import React from "react";
-import PeopleData from "./components/PeopleData";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PeopleCard from "./components/PeopleCard";
 
 export default function App() {
+    console.log("Peopledata");
+    const [data, setData] = useState();
+    useEffect(() => {
+        axios
+            .get("https://swapi.co/api/people")
+            .then(response => {
+                console.log(response.data.results);
+                setData(response.data.results);
+            })
+            .catch(error => console.log(error));
+    }, []);
+    console.log(data);
+    if (!data) return <h2>Loading .....</h2>;
     return (
         <div className="App">
-            <PeopleData />
+            {data.map((person, index) => {
+                return (
+                    <PeopleCard
+                        key={index}
+                        name={person.name}
+                        gender={person.gender}
+                        birthYear={person.birth_year}
+                        height={person.height}
+                        mass={person.mass}
+                    />
+                );
+            })}
         </div>
     );
 }
--- starwars/src/components/PeopleCard.js
+++ starwars/src/components/PeopleCard.js
@@ -2,14 +2,13 @@ import React from "react";
 
 const PeopleCard = props => {
     return (
-        <div className="PeopleList" key={props.key}>
+        <div className="PeopleList">
+            <div className="hidden" key={props.key} />
             <h2>Name: {props.name}</h2>
             <p>Gender: {props.gender}</p>
             <p>Birth Year: {props.birthYear}</p>
-            <p>Species: {props.species}</p>
             <p>Height: {props.height}</p>
             <p>Mass: {props.mass}</p>
-            <p>Home World: {props.homeworld}</p>
         </div>
     );
 };
