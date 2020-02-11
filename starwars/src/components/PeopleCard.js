de5c100ef6e5868e55fa16311262c0affdbc3aea
Ash W <amsheppard2016@gmail.com>
2020-02-10 22:15:35 -0500

    completed mvp

--- starwars/src/components/PeopleCard.js
+++ starwars/src/components/PeopleCard.js
@@ -1,15 +1,45 @@
 import React from "react";
+import styled from "styled-components";
+
+const PeopleList = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 2% 4%;
+    border: 2px solid black;
+    padding: 2% 2%;
+`;
+const PeopleName = styled.h1``;
+const RightLeftBox = styled.div`
+    display: flex;
+`;
+const LeftSide = styled.div`
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+`;
+
+const RightSide = styled.div`
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+`;
 
 const PeopleCard = props => {
     return (
-        <div className="PeopleList">
+        <PeopleList>
             <div className="hidden" key={props.key} />
-            <h2>Name: {props.name}</h2>
-            <p>Gender: {props.gender}</p>
-            <p>Birth Year: {props.birthYear}</p>
-            <p>Height: {props.height}</p>
-            <p>Mass: {props.mass}</p>
-        </div>
+            <PeopleName>Name: {props.name}</PeopleName>
+            <RightLeftBox>
+                <LeftSide>
+                    <h3>Gender: {props.gender}</h3>
+                    <h3>Birth Year: {props.birthYear}</h3>
+                </LeftSide>
+                <RightSide>
+                    <h3>Height: {props.height}</h3>
+                    <h3>Mass: {props.mass}</h3>
+                </RightSide>
+            </RightLeftBox>
+        </PeopleList>
     );
 };
 export default PeopleCard;
